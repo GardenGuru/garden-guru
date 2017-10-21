2c51e19b516f137c012c5d71ce182fda0a0a8a3f
Travis Meyer <tk_meyer@aol.com>
2017-10-21 16:07:51 -0500

    Added header and footer partial

---
+++ partials/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="/js/Chart.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="/js/dataTables.bootstrap4.js"></script>
+    <script type="text/javascript" src="/js/sb-admin.min.js"></script>
+    <script type="text/javascript" src="/js/sb-admin-datatables.min.js"></script>
+    <script type="text/javascript" src="/js/sb-admin-charts.min.js"></script>
+</footer>
